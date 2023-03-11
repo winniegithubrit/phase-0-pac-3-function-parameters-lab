7a2b1cee6fb44f7847633e7f70f796a090b88e36
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-11 22:50:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-function introduction(name){
-  return 'Hi, my name is ${name}.';
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
 }
-function introductionWithLanguage(name, language){
-return 'Hi, my name is ${name} and I am learning to program in ${language}.';
+
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-function introductionWithLanguageOptional(name, language = "JavaScript"){
-  return 'Hi, my name is ${name} and I am learning to program in ${language}.';
-}
-function introductionWithLanguageOptional(name, language = "JavaScript"){
-  return 'Hi, my name is ${name} and I am learning to program in ${language}.';
+
+function introductionWithLanguageOptional(name, language="JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
